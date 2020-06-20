745671d7af7f16718903e664390837501da8711d
Mahen Gandhi <mahengandhi19@gmail.com>
2020-06-20 10:31:49 +0530

    deleted

--- AudtoText.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer()
-audiofile = sr.AudioFile('audioname.wav')
-
-with audiofile as source:
-    audio = r.record(source)
-
-print(r.recognize_google(audio))
--- VidtoAud.py
+++
@@ -1,6 +0,0 @@
-import moviepy.editor as mp
-
-clip = mp.VideoFileClip("videoname.m4v").subclip(0, 50)
-clip.audio.write_audiofile("audioname.wav")
-
-# Audio will be in current directory
