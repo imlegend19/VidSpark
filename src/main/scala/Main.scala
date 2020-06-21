4c8615fb7de718ad9b893e680546f39c88be443f
Mahen Gandhi <mahengandhi19@gmail.com>
2020-06-21 15:15:34 +0530

    Made some changes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,44 +1,55 @@
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.log4j._
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.elasticsearch.spark.sql._
 
 class SetUpVidSpark(val spark: SparkSession, val path: String) {
+  def remove(num: Int, list: Seq[Int]): Seq[Int] = list diff List(num)
+
   def initDataset(): Unit = {
     val df: DataFrame = spark.read
       .format("csv")
       .option("header", "true")
       .load(path)
 
-    df.createOrReplaceTempView("data")
+//    df.createOrReplaceTempView("data")
 
-    val ids: Seq[Int] = df.select("id")
+    var ids: Seq[Int] = df.select("id")
       .distinct()
+      .rdd
+      .map(r => {
+        if (r.getString(0) != "ID") r.getString(0).toInt else -1
+      })
       .collect()
-      .map(r => r.getString(0).toInt)
       .toList
 
-    val vidTextSchema = List(
-      StructField("id", IntegerType, nullable = false),
-      StructField("text", StringType, nullable = false)
-    )
+    ids = remove(-1, ids)
 
-    val vidText = Seq[Row]()
+    println("Saving...")
+    df.saveToEs("spark/vidspark")
 
-    for (id <- ids) {
-      val msg = df.filter("id = %d".format(id))
-        .select("msg")
-        .collect()
-        .map(r => r.getString(0))
-        .toList
-        .mkString(" ")
-
-      vidText :+ Row(id, msg)
-    }
-
-    val vidTextDf = spark.createDataFrame(
-      spark.sparkContext.parallelize(vidText),
-      StructType(vidTextSchema)
-    )
+//    val vidTextSchema = List(
+//      StructField("id", IntegerType, nullable = false),
+//      StructField("text", StringType, nullable = false)
+//    )
+//
+//    val vidText = Seq[Row]()
+//
+//    for (id <- ids) {
+//      val msg = df.filter("id = %d".format(id))
+//        .select("msg")
+//        .collect()
+//        .map(r => r.getString(0))
+//        .toList
+//        .mkString(" ")
+//
+//      vidText :+ Row(id, msg)
+//    }
+//
+//    val vidTextDf = spark.createDataFrame(
+//      spark.sparkContext.parallelize(vidText),
+//      StructType(vidTextSchema)
+//    )
+//
 
     // TODO: Generate topics for vidTextDf
   }
@@ -54,11 +65,14 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     println("Initialising spark...")
+
     val spark = SparkSession
       .builder()
       .appName("VidSpark")
       .master("local")
       .getOrCreate()
+
+    spark.conf.set("es.index.auto.create", "true")
 
     println("Initialised!")
 
